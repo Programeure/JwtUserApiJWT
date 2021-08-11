d55cad50942ae4e08f9c0c97e2a4bf9c3f14da4d
Programeure <programmeurjava93@gmail.com>
2021-08-10 17:22:53 -0700

    Commit

---
+++ src/main/java/com/javadev/jwtUserApi/JwtUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.javadev.jwtUserApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUserApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtUserApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javadev/jwtUserApi/JwtUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.jwtUserApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtUserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
